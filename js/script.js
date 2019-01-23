147e3365dc65c08d8f9993ca9dde15af6c9e2d7a
Shweta Majumder <shweta_majumder@brown.edu>
2019-01-22 21:37:52 -0500

    updates script and markup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // change cursor when clicked
 // change random behaviours of first image through cursor
 // random behaviours - brightness, contrast, zoom
+
+window.onload = function() {
+  var img = document.getElementsByClassName("img");
+  var div = document.getElementsByTagName("div");
+  var button = document.getElementsByClassName("btn");
+  // console.log(body[1]);
+  function once() {
+    console.log("Done.");
+  }
+
+  button[0].addEventListener("click", once);
+}
