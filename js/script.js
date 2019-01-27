855bac670c3b4d8843bf8b49078f24bb3bfd873e
Shweta Majumder <shweta_majumder@brown.edu>
2019-01-27 17:37:38 -0500

    updates the zoom buttons

--- js/script.js
+++ js/script.js
@@ -8,40 +8,41 @@ window.onload = function() {
   var button = document.getElementsByClassName("btn");
 
   // change cursor with clicks
+  // ind = 0 => zoomin; ind=1 => zoom out
+  // n = 0 => clicked once; n = 1 => clicked twice
+  // m=0 => zoomin; m=1 => zoomout; m=2 => unselected
   var n = 0;
-  var m;
+  var m = 2;
   function zoom(ind) {
     if (n===0) {
+      button[ind].style.border = "2px white solid";
       if (ind===0) {
-        console.log("zoomin");
-        m = 1; //indicator for zoomIn
+        m = 0; //indicator for zoomIn
         body[0].style.cursor = "zoom-in";
       }
       else {
-        console.log("zoomout");
-        m = 2; //indicator for zoomOut
+        m = 1; //indicator for zoomOut
         body[0].style.cursor = "zoom-out";
       }
       n = 1;
     }
     else {
-      console.log("donothing");
-      body[0].style.cursor = "default";
-      n=0;
-      m=0;
+      if (m===ind) {
+        button[ind].style.border = "none";
+        body[0].style.cursor = "default";
+        n=0;
+        m=2;
+      }
+      else {
+        button[0].style.border = "none";
+        button[1].style.border = "none";
+        n = 0;
+        m = ind;
+        zoom(ind);
+      }
     }
   }
-  // function zoomOut() {
-  //   m = 2; //indicator for zoomOut
-  //   if (n===0) {
-  //     body[0].style.cursor = "zoom-out";
-  //     n = 1;
-  //   }
-  //   else {
-  //     body[0].style.cursor = "default";
-  //     n=0;
-  //   }
-  // }
+
 
   // event listener for zoom in and out
   button[0].addEventListener("click", function zoomIn() {zoom(0);});
