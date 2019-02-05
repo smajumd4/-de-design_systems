e4663aacad510b020a47a5fb5bcf4d36bd0af691
Shweta Majumder <shweta_majumder@brown.edu>
2019-02-05 03:12:23 -0500

    updates layout and adds functionalities

--- js/script.js
+++ js/script.js
@@ -26,7 +26,6 @@ window.onload = function() {
         body[0].style.cursor = "zoom-out";
       }
       n = 1;
-      console.log("m is: ", m)
     }
     else {
       if (m===ind) {
@@ -38,7 +37,7 @@ window.onload = function() {
         m=2;
       }
       else {
-        $(".btn").css({"border":"none", "width": "18vw", "height":"12vw", "box-shadow": "none"});
+        $(".btn").css({"border":"none", "width": "18vw", "box-shadow": "none"});
         n = 0;
         m = ind;
         zoom(ind);
@@ -51,7 +50,11 @@ window.onload = function() {
   $(".grid-block").click(function(){
     if (m===0) {
       x = x + 6;
-      $(".shape").css({"transform": `translateX(${x}vh)`});
+      $(".shape").each(function(i) {
+        var y = Math.floor(Math.random() * Math.floor(8));
+        console.log(y);
+        $(this).css({"transform": `translateX(${x+y}vh)`});
+      })
       if (x>= 70) {
         $(".shape").css({"transform": `translateX(-4vh)`});
         x = 0;
@@ -59,7 +62,11 @@ window.onload = function() {
     }
     if (m===1) {
       x = x - 6;
-      $(".shape").css({"transform": `translateX(${x}vh)`});
+      $(".shape").each(function(i) {
+        var y = Math.floor(Math.random() * Math.floor(8));
+        console.log(y);
+        $(this).css({"transform": `translateX(${x-y}vh)`});
+      })
       if (x<=-4) {
         x = x + 72;
         $(".shape").css({"transform": `translateX(${x}vh)`});
@@ -67,25 +74,90 @@ window.onload = function() {
     }
   });
 
-  // hover over the block
+  // move the block
+  var z = 0;
+  $(".shape").click(function(){
+    if (m===0) {z = z + 6;}
+    if (m===1) {z = z - 6;}
+    $(".grid-block").css({'transform': `rotate(${z}deg)`});
+  });
 
+  // hover over the block
   $(".grid-block").hover(
     function(){
-      $(".shape").css({"background-color": "#D62A00", "box-shadow": "0.1vw .4vw .8vw 0.2vw #FF7654"});
+      $(".shape").css({"background-color": "#1E3F3F", "box-shadow": "0.1vw .4vw .8vw 0.2vw #FF7654"});
     }, function(){
       $(".shape").css({"background-color": "#D62A00", "box-shadow": "none"});
+    }
+  );
+
+  // hover over the shape
+  $(".shape").hover(
+    function(){
+      $(".grid-block").css({"background-color": "#D62A00", "box-shadow": "0.1vw .4vw .8vw 0.2vw #1C8989"});
+    }, function(){
+      $(".grid-block").css({"background-color": "#1E3F3F", "box-shadow": "none"});
     }
   );
 
   // hover over the image
   $(".img").hover(
     function(){
-      $(this).css({"background-color": "#D62A00", "box-shadow": "0.1vw .4vw .8vw 0.2vw #331D3F, 0.2vw .6vw .7vw 0.3vw #8E5572"});
+      $(this).css({"box-shadow": "0.1vw .4vw .8vw 0.2vw #331D3F, 0.2vw .6vw .7vw 0.3vw #8E5572"});
     }, function(){
-      $(this).css({"background-color": "#D62A00", "box-shadow": "none"});
+      $(this).css({"box-shadow": "none"});
     }
   );
 
+
+  var topofDiv = $("#alert-container").offset().top; //gets offset of alert
+
+  // scroll event
+  $(window).scroll(function(){
+    if (m===2) {
+      if ($(window).scrollTop() >= (topofDiv)) {
+        $('.alert > p').css({'color': '#BA2500'});
+      }
+      else {
+        $('.alert > p').css({'color': '#CCC8CE'});
+      }
+    }
+    else {
+      $('.alert > p').css({'color': '#CCC8CE'});
+    }
+  });
+
+  // image property change
+  // img-1
+  var brit = 100;
+  $(".img-1").click(function(){
+    if (m===0) {
+      brit = brit + 20;
+      $(".img-1").css({'filter': `brightness(${brit}%)`});
+      $(".notes-1 > p").text(`Brightness changed to ${brit}%`)
+    }
+    if (m===1) {
+      console.log('img-1 clicked');
+      cont = cont - 10;
+      $(".img-1").css({'filter': `brightness(${brit}%)`});
+      $(".notes-1 > p").text(`Brightness changed to ${brit}%`)
+    }
+  });
+  // img-2
+  var cont = 100;
+  $(".img-2").click(function(){
+    if (m===0) {
+      cont = cont + 20;
+      $(".img-2").css({'filter': `contrast(${cont}%)`});
+      $(".notes-2 > p").text(`Contrast changed to ${cont}%`)
+    }
+    if (m===1) {
+      console.log('img-2 clicked');
+      cont = cont - 10;
+      $(".img-2").css({'filter': `contrast(${cont}%)`});
+      $(".notes-2 > p").text(`Contrast changed to ${cont}%`)
+    }
+  });
 
 
   // event listener for zoom in and out
